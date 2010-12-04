8031e07326e209c875b67f5db0706ea457a7b07d
David Paul Christensen <dpchrist@holgerdanske.com>
2010-12-04 22:07:45 +0000

    Import of DPCHRIST/Dpchrist-ExtUtils-MakeMaker-1.016 from CPAN.
    gitpan-cpan-distribution: Dpchrist-ExtUtils-MakeMaker
    gitpan-cpan-version:      1.016
    gitpan-cpan-path:         DPCHRIST/Dpchrist-ExtUtils-MakeMaker-1.016.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

--- lib/Dpchrist/ExtUtils/MakeMaker.pm
+++ lib/Dpchrist/ExtUtils/MakeMaker.pm
@@ -1,5 +1,5 @@
 #######################################################################
-# $Id: MakeMaker.pm,v 1.15 2010-12-02 22:54:44 dpchrist Exp $
+# $Id: MakeMaker.pm,v 1.16 2010-12-04 21:43:12 dpchrist Exp $
 #######################################################################
 # package:
 #----------------------------------------------------------------------
@@ -11,7 +11,7 @@ use constant DEBUG		=> 0;
 use strict;
 use warnings;
 
-our $VERSION  = sprintf "%d.%03d", q$Revision: 1.15 $ =~ /(\d+)/g;
+our $VERSION  = sprintf "%d.%03d", q$Revision: 1.16 $ =~ /(\d+)/g;
 
 #######################################################################
 # uses:
@@ -61,7 +61,7 @@ Dpchrist::ExtUtils::MakeMaker - additional Makefile targets and rules
 
 =head1 DESCRIPTION
 
-This documentation describes module revision $Revision: 1.15 $.
+This documentation describes module revision $Revision: 1.16 $.
 
 
 This is alpha test level software
